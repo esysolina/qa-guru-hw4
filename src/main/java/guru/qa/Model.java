853c43f28c0b22cc100539821ddbb9c5a44137f2
esysolina <evelina.ova@gmail.com>
2022-01-07 22:25:07 +0500

    fix

--- src/main/java/guru/qa/Model.java
+++ src/main/java/guru/qa/Model.java
@@ -25,34 +25,8 @@ public class Model {
             System.out.println(favToy);
         }
 
-        public void goToPlace(){
+        public void goToPlace() {
             place = true;
         }
-    }
-
-    public static void main(String[] args) {
-        Dog fido = new Dog();
-        fido.name = "Fido";
-        fido.breed = "Beagle";
-        fido.age = 3;
-        fido.weight = 13;
-        fido.stepOnScale();
-        fido.eatLess();
-        fido.stepOnScale();
-        fido.favToy = "ball";
-        fido.bringToy();
-        fido.bark();
-
-        Dog max = new Dog();
-        max.name = "Maximus";
-        max.breed = "Golden Retriever";
-        max.age = 7;
-        max.weight = 32;
-        max.favToy = "rabbit";
-        max.bark();
-        max.stepOnScale();
-        max.bringToy();
-        max.goToPlace();
-        System.out.println(max.place);
     }
 }
